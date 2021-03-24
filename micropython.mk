3889d5b82bc7aeba8047f4280b6e04ec90a0e691
Stepan Snigirev <snigirev.stepan@gmail.com>
2021-03-24 19:33:04 +0100

    get rid of errors, enable by default

--- micropython.mk
+++ micropython.mk
@@ -6,4 +6,4 @@ SRC_USERMOD += $(SECP256K1_MODULE_DIR)/mpy/config/ext_callbacks.c
 SRC_USERMOD += $(SECP256K1_MODULE_DIR)/mpy/libsecp256k1.c
 
 # We can add our module folder to include paths if needed
-CFLAGS_USERMOD += -I$(SECP256K1_MODULE_DIR)/secp256k1 -I$(SECP256K1_MODULE_DIR)/secp256k1/src -I$(SECP256K1_MODULE_DIR)/mpy/config -DHAVE_CONFIG_H -Wno-unused-function
+CFLAGS_USERMOD += -I$(SECP256K1_MODULE_DIR)/secp256k1 -I$(SECP256K1_MODULE_DIR)/secp256k1/src -I$(SECP256K1_MODULE_DIR)/mpy/config -DHAVE_CONFIG_H -Wno-unused-function -Wno-error -DMODULE_SECP256K1_ENABLED=1
